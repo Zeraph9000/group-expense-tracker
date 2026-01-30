ada501ad04ce505ea8c50f4b67f208daaa25aae6
Andrew <z5621138@ad.unsw.edu.au>
2026-01-30 22:24:31 +0700

    added groups

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -27,12 +27,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    GroupsModule
   ],
   controllers: [AppController],
   providers: [AppService],
